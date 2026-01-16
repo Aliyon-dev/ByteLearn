51b02f888e70330076440692e67064ef17df7aa8
aliyon-dev <temboalion@gmail.com>
2026-01-16 20:15:08 +0200

    commit tracked

---
+++ backend/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Create superuser if it doesn't exist (optional)
+# python manage.py createsuperuser --noinput || true
+
+# Collect static files
+python manage.py collectstatic --noinput
